eeb8408c5b138ccf5bc5e40dc155db55f80c22ee
Aleksandr Kretov <kretov.1988@gmail.com>
2022-04-19 10:02:55 +0500

    Initial Commit

---
+++ CoreDataDemoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreDataDemoApp
+//
+//  Created by Aleksandr Kretov on 19.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
