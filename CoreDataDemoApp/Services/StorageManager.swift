48e0a180f0aab833f0fc69c2411144c0ebcabe42
Aleksandr Kretov <kretov.1988@gmail.com>
2022-04-20 22:10:20 +0500

    Almost Done

--- CoreDataDemoApp/Services/StorageManager.swift
+++ CoreDataDemoApp/Services/StorageManager.swift
@@ -5,37 +5,70 @@
 //  Created by Aleksandr Kretov on 19.04.2022.
 //
 
-import Foundation
 import CoreData
+import Foundation
 
 class StorageManager {
-    
     static let shared = StorageManager()
-    
+
     // MARK: - Core Data stack
 
     var persistentContainer: NSPersistentContainer = {
         let container = NSPersistentContainer(name: "CoreDataDemoApp")
-        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+        container.loadPersistentStores(completionHandler: { _, error in
             if let error = error as NSError? {
                 fatalError("Unresolved error \(error), \(error.userInfo)")
             }
         })
         return container
     }()
-    
+
     private init() {}
 
     // MARK: - Core Data Saving support
 
-    func saveContext () {
-        let context = persistentContainer.viewContext
+    func saveContext() {
+        let context = self.persistentContainer.viewContext
         if context.hasChanges {
             do {
                 try context.save()
             } catch {
                 let nserror = error as NSError
                 fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
+    }
+
+    func fetchData() -> [ToDoTask] {
+        var taskList: [ToDoTask] = []
+        let fetchRequest = ToDoTask.fetchRequest()
+        do {
+            taskList = try self.persistentContainer.viewContext.fetch(fetchRequest)
+        } catch {
+            print(error.localizedDescription)
+        }
+        return taskList
+    }
+
+    func save(task: ToDoTask) {
+        self.persistentContainer.viewContext.insert(task)
+        if self.persistentContainer.viewContext.hasChanges {
+            do {
+                try self.persistentContainer.viewContext.save()
+            } catch {
+                print(error)
+            }
+        }
+        saveContext()
+    }
+
+    func delete(task: ToDoTask) {
+        self.persistentContainer.viewContext.delete(task)
+        if self.persistentContainer.viewContext.hasChanges {
+            do {
+                try self.persistentContainer.viewContext.save()
+            } catch {
+                print(error)
             }
         }
     }
--- CoreDataDemoApp/ViewControllers/TasksListViewController.swift
+++ CoreDataDemoApp/ViewControllers/TasksListViewController.swift
@@ -7,14 +7,10 @@
 
 import UIKit
 
-protocol TasksListViewControllerDelegate {
-    func reloadData()
-}
-
 class TasksListViewController: UITableViewController {
-    
     private var tasksList: [ToDoTask] = []
-    private var cellID = "cell"
+
+    private let cellID = "cell"
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,9 +20,13 @@ class TasksListViewController: UITableViewController {
         fetchData()
     }
 
+    private func fetchData() {
+        tasksList = StorageManager.shared.fetchData()
+    }
+
     private func setupNavBar() {
         title = "Actual Tasks"
-        navigationController?.navigationBar.prefersLargeTitles =  true
+        navigationController?.navigationBar.prefersLargeTitles = true
         let navBarAppearence = UINavigationBarAppearance()
         navBarAppearence.configureWithOpaqueBackground()
         navBarAppearence.backgroundColor = .systemTeal
@@ -37,31 +37,58 @@ class TasksListViewController: UITableViewController {
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addTask))
         navigationController?.navigationBar.tintColor = .white
     }
-    
+
     @objc private func addTask() {
-        let newTaskVC = NewTaskViewController()
-        newTaskVC.delegate = self
-        present(newTaskVC, animated: true)
+        alert(title: "New Task", message: "Enter new task name", atIndex: nil)
     }
-    
-    private func fetchData() {
-        let viewContext = StorageManager.shared.persistentContainer.viewContext
-        let fetchRequest = ToDoTask.fetchRequest()
-        do {
-            tasksList = try viewContext.fetch(fetchRequest)
-            
-        } catch {
-            print(error.localizedDescription)
+
+    private func alert(title: String, message: String, atIndex: Int?) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alert.addTextField { textfield in
+            textfield.placeholder = "Enter new task name"
         }
+        if let index = atIndex {
+            let updateAction = UIAlertAction(title: "Update", style: .default) { _ in
+                guard let taskName = alert.textFields?.first?.text else { return }
+                self.update(taskName: taskName, at: index)
+            }
+            alert.textFields?.first?.text = tasksList[index].title
+            alert.addAction(updateAction)
+        } else {
+            let addAction = UIAlertAction(title: "Save", style: .default) { _ in
+                guard let taskName = alert.textFields?.first?.text else { return }
+                self.save(newTaskName: taskName)
+            }
+            alert.addAction(addAction)
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: .destructive)
+
+        alert.addAction(cancelAction)
+        present(alert, animated: true)
+    }
+
+    func save(newTaskName: String) {
+        let task = ToDoTask(context: StorageManager.shared.persistentContainer.viewContext)
+        task.title = newTaskName
+        StorageManager.shared.save(task: task)
+        fetchData()
+        let cellIndex = IndexPath(row: tasksList.count - 1, section: 0)
+        tableView.insertRows(at: [cellIndex], with: .automatic)
+    }
+
+    private func update(taskName: String, at index: Int) {
+        tasksList[index].title = taskName
+        let indexPath = IndexPath(row: index, section: 0)
+        StorageManager.shared.saveContext()
+        tableView.reloadRows(at: [indexPath], with: .automatic)
     }
 }
 
 extension TasksListViewController {
-
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         tasksList.count
     }
-    
+
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: cellID, for: indexPath)
         let task = tasksList[indexPath.row]
@@ -70,11 +97,26 @@ extension TasksListViewController {
         cell.contentConfiguration = content
         return cell
     }
-}
 
-extension TasksListViewController: TasksListViewControllerDelegate {
-    func reloadData() {
-        fetchData()
-        tableView.reloadData()
+    override func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        let action = UIContextualAction(style: .destructive, title: "Delete") { _, _, _ in
+            let task = self.tasksList[indexPath.row]
+            StorageManager.shared.delete(task: task)
+            self.fetchData()
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+        }
+        return UISwipeActionsConfiguration(actions: [action])
+    }
+
+    override func tableView(_ tableView: UITableView, leadingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        let action = UIContextualAction(style: .normal, title: "Edit") { _, _, _ in
+            guard let title = self.tasksList[indexPath.row].title else { return }
+            self.alert(title: title, message: "Edit your task name", atIndex: indexPath.row)
+        }
+        return UISwipeActionsConfiguration(actions: [action])
+    }
+
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        50
     }
 }
